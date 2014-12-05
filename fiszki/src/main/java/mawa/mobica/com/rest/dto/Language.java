2da4cf227eaac3f6bedb7d2c17cfb8f202fa23a7
Marcin Wachowiak <marcin.wachowiak@mobica.com>
2014-12-05 11:40:05 +0100

    unit tests for jaxws

--- fiszki/src/main/java/mawa/mobica/com/rest/dto/Language.java
+++ fiszki/src/main/java/mawa/mobica/com/rest/dto/Language.java
@@ -35,6 +35,45 @@ public class Language implements Serializable{
 			this.description = description;
 		}
 
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((getDescription() == null) ? 0 : getDescription().hashCode());
+			result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+			result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (!(obj instanceof Language))
+				return false;
+			Language other = (Language) obj;
+			if (getDescription() == null) {
+				if (other.getDescription() != null)
+					return false;
+			} else if (!getDescription().equals(other.getDescription()))
+				return false;
+			if (getId() == null) {
+				if (other.getId() != null)
+					return false;
+			} else if (!getId().equals(other.getId()))
+				return false;
+			if (getName() == null) {
+				if (other.getName() != null)
+					return false;
+			} else if (!getName().equals(other.getName()))
+				return false;
+
+			return true;
+		}
+
 		@Override
 		public String toString() {
 			return new StringBuilder().
